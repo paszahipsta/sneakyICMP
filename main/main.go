df3f0d8f8604e68ec0ed10655005c0e7a3dc20e2
Mateusz Paszynski <pasmat@proton.me>
2022-08-18 10:36:12 +0200

    slowly working on removing dependency on go-ping library

---
+++ main/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"sneakyicmp/sneakyicmp"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+)
+
+var body = &icmp.Echo{
+	ID:   123123,
+	Seq:  100,
+	Data: []byte("123"),
+}
+
+var xd = icmp.Message{
+	Type: ipv4.ICMPTypeEcho,
+	Code: 0,
+	Body: body,
+}
+
+func main() {
+	var msg string
+	var mode bool
+	flag.StringVar(&msg, "m", "sneakyleaky", "smt easy")
+	flag.BoolVar(&mode, "u", false, "smt easy")
+
+	flag.Parse()
+	sneakyicmp.SendICMP(xd, "0.0.0.0")
+	/*if !mode {
+		//sneakyicmp.SendSneakyMessage("172.31.252.17", msg)
+	} else {
+		sneakyicmp.RecvSneakyMessage("172.31.252.17")
+	}
+	*/
+}
--- sneakyicmp/sneakyicmp.go
+++ sneakyicmp/sneakyicmp.go
@@ -2,6 +2,9 @@ package sneakyicmp
 
 import (
 	"log"
+	"net"
+	"strconv"
+	"strings"
 
 	"github.com/go-ping/ping"
 	"golang.org/x/net/icmp"
@@ -13,13 +16,62 @@ const (
 	icmpHeader       = 8
 )
 
+var body = &icmp.Echo{
+	ID:   123123,
+	Seq:  100,
+	Data: []byte("213"),
+}
+
+var xd = icmp.Message{
+	Type: ipv4.ICMPTypeEcho,
+	Code: 0,
+	Body: body,
+}
+
 func formatMessage(message string) []byte {
 	x := []byte(message)
 	return x
 }
 
+/*
+Based on ip in string, return a pointer to object net.IPAddr
+*/
+func ipStringToIpAddr(ip string) *net.IPAddr {
+	ipOctects := strings.Split(ip, ".")
+	var ipByteOctets [4]byte
+	for i := 0; i < len(ipOctects); i++ {
+		ipInt, err := strconv.Atoi(ipOctects[i])
+		if err != nil {
+			log.Fatalf("Wrong address!")
+		}
+		ipByteOctets[i] = byte(ipInt)
+	}
+	ipv4 := net.IPv4(ipByteOctets[0], ipByteOctets[1], ipByteOctets[2], ipByteOctets[3])
+	targetIp := &net.IPAddr{
+		IP: ipv4,
+	}
+	return targetIp
+}
+
+func SendICMP(msg icmp.Message, dst string) {
+	c, err := net.ListenPacket("ip:1", dst)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pkt, err := msg.Marshal(nil)
+	if err != nil {
+		log.Fatal("Failed to fetch message")
+	}
+
+	targetIp := ipStringToIpAddr(dst)
+	x, err := c.WriteTo(pkt, targetIp)
+	log.Println(err)
+	log.Println(x)
+}
+
 func SendSneakyMessage(addr string, message string) error {
 	p := ping.New(addr)
+
 	fMessage := formatMessage(message)
 	for i := 0; i < len(message); i++ {
 		p.Count = 1
