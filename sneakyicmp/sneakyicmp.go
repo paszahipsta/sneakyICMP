24dbb83ede77641d5432ea3529a7c3d27067805a
Mateusz Paszynski <pasmat@proton.me>
2022-08-31 23:04:42 +0200

    redesigned the code

--- sneakyicmp/sneakyicmp.go
+++ sneakyicmp/sneakyicmp.go
@@ -2,8 +2,8 @@ package sneakyicmp
 
 import (
 	"log"
-	"net"
 
+	"github.com/go-ping/ping"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 )
@@ -13,126 +13,39 @@ const (
 	icmpHeader       = 8
 )
 
-func formatMessage(message string) []byte {
-	x := []byte(message)
-	return x
+type sneakyMessage struct {
+	privileged bool
+	data       []byte
 }
 
-type Ping struct {
-	Privileged bool
-	Size       int
-	DstIp      string
-	SrcIp      string
-	Protocol   string
+func New(privileged bool, data []byte) *sneakyMessage {
+	sMsg := sneakyMessage{
+		privileged: privileged,
+		data:       data,
+	}
+	return &sMsg
 }
 
-type Message struct {
-	payload       []Ping
-	encryption    bool
-	encryptionKey string
+func NewEncrypted(privileged bool, data []byte, pgpKey string) *sneakyMessage {
+	sMsg := sneakyMessage{
+		privileged: privileged,
+		data:       encryptData(data),
+	}
+	return &sMsg
 }
 
-/*
-Based on ip in string, return a pointer to object net.IPAddr
-*/
-func ipStringToIpAddr(ip string) *net.IPAddr {
+func SendSneakyMessage(dst string, sMsg *sneakyMessage) error {
+	p := ping.New(dst)
 
-	ipv4 := net.ParseIP(ip)
-	targetIp := &net.IPAddr{
-		IP: ipv4,
-	}
-	return targetIp
-}
-
-func NewPing(dstIp string, srcIp string, elevated bool) *Ping {
-	p := Ping{
-		Privileged: elevated,
-		DstIp:      dstIp,
-		SrcIp:      srcIp,
+	if sMsg.privileged {
+		p.SetPrivileged(true)
 	}
 
-	return p
-}
-
-func (p *Ping) privileged(elevated bool) {
-	if elevated == true {
-		p.Privileged = true
-		p.Protocol = "ip:icmp"
-	} else {
-		p.Privileged = false
-		p.Protocol = "udp4"
-	}
-}
-
-func (p *Ping) makeConn() (net.PacketConn, error) {
-	conn, err := net.ListenPacket(p.Protocol, p.DstIp)
-	if err != nil {
-		return nil, err
-	}
-	return conn, nil
-}
-
-func (p *Ping) createICMP() ([]byte, error) {
-
-	body := &icmp.Echo{
-		ID:   123123,
-		Seq:  100,
-		Data: []byte("123213123"),
-	}
-
-	icmpPacket := icmp.Message{
-		Type: ipv4.ICMPTypeEcho,
-		Code: 0,
-		Body: body,
-	}
-
-	pkt, err := icmpPacket.Marshal(nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return pkt, nil
-}
-
-func (p *Ping) SendICMP() {
-	//Create icmp packet based on ping struct
-	icmpPacket, err := p.createICMP()
-	if err != nil {
-		log.Fatalf("Failed to fetch message, %s", err)
-	}
-	//Make connection with host
-	c, err := p.makeConn()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//Send packet to host
-	targetIp := ipStringToIpAddr(p.DstIp)
-	_, err = c.WriteTo(icmpPacket, targetIp)
-	if err != nil {
-		log.Print(err)
-	}
-}
-
-/*
-func SendSneakyMessage(addr string, message string) error {
-	p := ping.New(addr)
-
-	fMessage := formatMessage(message)
-	for i := 0; i < len(message); i++ {
-		p.Count = 1
-		p.Size = int(fMessage[i])
-		p.Run()
-	}
-
-	//Send size 256 to finish transmission
-	p.Count = 1
-	p.Size = 256
-	p.Run()
+	sMsg.sendPings(p)
 
 	return nil
 }
-*/
+
 func RecvSneakyMessage(url string) []byte {
 	var sneakyMessage []byte
 	conn, err := icmp.ListenPacket("ip4:icmp", url)
@@ -166,4 +79,25 @@ func RecvSneakyMessage(url string) []byte {
 		}
 	}
 
+}
+
+func (sMsg sneakyMessage) sendPings(p *ping.Pinger) {
+	for i := 0; i < len(sMsg.data); i++ {
+		p.Count = 1
+		p.Size = int(sMsg.data[i])
+		p.Run()
+	}
+
+	//Send size 256 to finish transmission
+	p.Count = 1
+	p.Size = 256
+	p.Run()
+}
+
+func encryptData(data []byte) []byte {
+	//encrypt data with pgp key
+	log.Print("encrypt")
+	var x []byte
+	x = append(x, 1)
+	return x
 }
